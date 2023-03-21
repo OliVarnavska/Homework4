5aec147f9e8528f1bca1fc4e741b530954906227
Oli <oli.varnavska@gmail.com>
2023-03-21 23:29:43 +0800

    Add Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 25: Напишите цикл, который принимает 
+// на вход два числа (A и B) и возводит число A 
+// в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int Result(int A, int B)
+{
+    int a1 = 1;
+    for (int i=0; i < B; i++)
+    {
+        a1 *= A; 
+    }
+    return a1;
+}
+bool Exp(int B)
+{
+    if (B < 0)
+    {
+        Console.WriteLine("Показатель не должен быть меньше 0");
+        return false;
+    }
+    return true;
+}
+
+int A = Prompt("Введите основание: ");
+int B = Prompt("Введите показатель степени: ");
+ if (Exp(B))
+ {
+    Console.WriteLine($"Число {A} в степени {B} равно {Result(A, B)}");
+ }
+
+
